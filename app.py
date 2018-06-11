c363071dfb734367e050324a4df4efa5a0b46334
Ryan Hinkel <ryanhinkel@gmail.com>
2018-06-11 19:18:50 -0400

    Inline view declaration

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 
+from pyramid.view import view_config
+
 
 if __name__ == '__main__':
     with Configurator() as config:
@@ -9,6 +11,14 @@ if __name__ == '__main__':
         config.add_jinja2_renderer('.html')
 
         config.add_route('hello', '/')
+
+        @view_config(
+            route_name='hello',
+            renderer='json',
+        )
+        def hello(request):
+            return {'message': 'Hello'}
+
         config.scan()
 
         app = config.make_wsgi_app()
--- views/hello.py
+++
@@ -1,9 +0,0 @@
-from pyramid.view import view_config
-
-
-@view_config(
-    route_name='hello',
-    renderer='json',
-)
-def hello(request):
-    return {'message': 'Hello'}
